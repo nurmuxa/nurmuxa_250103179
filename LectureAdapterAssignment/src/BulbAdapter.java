e5a8ce8e62ef151fb01eff56cc538b1771ca774d
nurmuxa <trollnur123@gmail.com>
2026-09-23 16:19:51 +0600

    Update BulbAdapter.java

--- LectureAdapterAssignment/src/BulbAdapter.java
+++ LectureAdapterAssignment/src/BulbAdapter.java
@@ -1,3 +1,36 @@
-public class BulbAdapter {
-    
+public class BulbAdapter implements SmartDevice {
+    private final LegacyBulb bulb;
+
+    public BulbAdapter(LegacyBulb bulb) {
+        if (bulb == null) {
+            throw new IllegalArgumentException("Bulb can't be null!");
+        }
+
+        this.bulb = bulb;
+    }
+
+    @Override
+    public void turnOn(){
+        bulb.setBrightness(255);
+    }
+
+    @Override
+    public void turnOff(){
+        bulb.setBrightness(0);
+    }
+
+    @Override
+    public boolean isOn(){
+        if(bulb.hasPower()==true && bulb.readBrightness()>0){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getPowerPercent(){
+        int power = 0 ;
+        power = ((bulb.readBrightness()*100)/255);
+        return power;
+    }
 }
