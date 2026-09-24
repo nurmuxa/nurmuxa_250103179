9afa3a966a26f641346e4f9d094c767f3b3beca8
nurmuxa <trollnur123@gmail.com>
2026-09-25 01:28:33 +0600

    Update Main.java

--- LectureAdapterAssignment/src/Main.java
+++ LectureAdapterAssignment/src/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        LegacyBulb rawBulb = new LegacyBulb();
+        LegacyThermostat rawThermostat = new LegacyThermostat();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BulbAdapter bulbAdapter = new BulbAdapter(rawBulb);
+
+        ThermostatAdapter thermostatAdapter = new ThermostatAdapter(rawThermostat);
+
+        List<SmartDevice> deviceList = List.of(bulbAdapter,thermostatAdapter);
+
+        ModernHub hub = new ModernHub(deviceList);
+        hub.activateAll();
+
+        System.out.println(bulbAdapter.isOn());
+        System.out.println(hub.calculateAveragePowerUsage());
+
+
+        /*
+         * LegacyBulb does not implement the SmartDevice interface,
+         * so ModernHub cannot accept it directly.
+         * BulbAdapter implements SmartDevice and wraps LegacyBulb
+         * using composition without modifying the original class.
+         */
+
+        rawBulb.breakFilament();
+
+        System.out.println(rawBulb.readBrightness());
+        System.out.println(bulbAdapter.isOn());
+        System.out.println(bulbAdapter.getPowerPercent());
+
+
+        rawThermostat.rotateDial("STUCK");
+
+        System.out.println(thermostatAdapter.isOn());
+        System.out.println(thermostatAdapter.getPowerPercent());
+
+        rawThermostat.rotateDial(null);
+        System.out.println(thermostatAdapter.isOn());
+        System.out.println(thermostatAdapter.getPowerPercent());
+
+        hub.emergencyShutdown();
+        System.out.println(hub.calculateAveragePowerUsage());
+
+        System.out.println("is bulb on? : " + bulbAdapter.isOn());
+        System.out.println("is thermostat on? : " + thermostatAdapter.isOn());
+
+
+
+
+
     }
+
 }
